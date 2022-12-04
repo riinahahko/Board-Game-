9886033a0dbaf1b908c749e14e84cc25bdbecff2
riinahahko <114108520+riinahahko@users.noreply.github.com>
2022-12-04 16:23:06 +0200

    updated models.py

--- board_games/models.py
+++ board_games/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Game(models.Model):
+    """A game the user has added"""
+    name = models.CharField(max_lenght=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return a string reprresentation of the model"""
+        return self.name
